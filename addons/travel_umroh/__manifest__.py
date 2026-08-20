828db3da78b519dffdcae76a9e05e8b2ca2a2777
Ngurah Arya <ngurahpratama2002@gmail.com>
2026-08-20 16:10:59 +0700

    feat: scaffold travel umroh addon and roles

---
+++ addons/travel_umroh/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "Travel Umroh",
+    "summary": "Manage Umroh packages, departures, jamaah, and bookings",
+    "version": "18.0.1.0.0",
+    "category": "Services",
+    "license": "LGPL-3",
+    "depends": [
+        "base",
+        "contacts",
+        "mail",
+        "product",
+        "sale_management",
+        "account",
+    ],
+    "data": [
+        "security/travel_security.xml",
+        "security/ir.model.access.csv",
+    ],
+    "application": True,
+    "installable": True,
+}
