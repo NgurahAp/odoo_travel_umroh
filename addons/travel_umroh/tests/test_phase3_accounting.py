850feba7a3454109aa26c84fba768f719a6c3554
Ngurah Arya <ngurahpratama2002@gmail.com>
2026-08-22 11:59:26 +0700

    test: cover standard travel final settlement

--- addons/travel_umroh/tests/test_phase3_accounting.py
+++ addons/travel_umroh/tests/test_phase3_accounting.py
@@ -6,6 +6,35 @@ from .common import TravelAccountingCase
 
 @tagged("post_install", "-at_install")
 class TestTravelPaymentState(TravelAccountingCase):
+    def _pay_invoice(self, invoice):
+        (
+            self.env["account.payment.register"]
+            .with_user(self.finance)
+            .with_context(
+                active_model="account.move",
+                active_ids=invoice.ids,
+            )
+            .create(
+                {
+                    "journal_id": self.company_data[
+                        "default_journal_bank"
+                    ].id,
+                }
+            )
+            .action_create_payments()
+        )
+        invoice.invalidate_recordset(["amount_residual", "payment_state"])
+
+    def _create_final_invoice(self, order):
+        existing_invoices = order.invoice_ids
+        self._create_invoice_wizard(
+            order,
+            "delivered",
+            deduct_down_payments=True,
+        ).create_invoices()
+        order.invalidate_recordset(["invoice_ids", "invoice_status"])
+        return (order.invoice_ids - existing_invoices).ensure_one()
+
     def test_confirmed_booking_without_posted_invoice_is_unpaid(self):
         order = self._confirmed_booking("PAY-NONE")
 
@@ -64,6 +93,72 @@ class TestTravelPaymentState(TravelAccountingCase):
         )
         self._post_and_pay(invoice)
 
+        self.assertEqual(order.travel_payment_state, "dp")
+
+    def test_standard_final_invoice_and_payment_reach_paid_once(self):
+        order = self._confirmed_booking("PAY-FINAL", participant_count=2)
+        downpayment_invoice = self._create_downpayment_invoice(order)
+
+        downpayment_invoice.with_user(self.finance).action_post()
+        self.assertEqual(self.departure.reserved_seats, 0)
+        self._pay_invoice(downpayment_invoice)
+        self.assertEqual(self.departure.reserved_seats, 2)
+        self.assertEqual(order.travel_payment_state, "dp")
+
+        final_invoice = self._create_final_invoice(order)
+        participant_lines = order.participant_ids.sale_line_id
+        linked_sale_lines = final_invoice.invoice_line_ids.sale_line_ids
+        deducted_downpayment_lines = final_invoice.invoice_line_ids.filtered(
+            lambda line: line.sale_line_ids.filtered("is_downpayment")
+        )
+
+        self.assertTrue(participant_lines <= linked_sale_lines)
+        self.assertTrue(deducted_downpayment_lines)
+        self.assertEqual(
+            self.env.company.currency_id.compare_amounts(
+                final_invoice.amount_total,
+                order.amount_total - downpayment_invoice.amount_total,
+            ),
+            0,
+        )
+
+        self._post_and_pay(final_invoice)
+        order.invalidate_recordset(
+            ["invoice_ids", "invoice_status", "travel_payment_state"]
+        )
+        self.departure.invalidate_recordset(
+            ["reserved_seats", "remaining_seats", "is_full"]
+        )
+        order.invoice_ids.invalidate_recordset(
+            ["amount_residual", "payment_state"]
+        )
+
+        self.assertEqual(order.invoice_status, "invoiced")
+        self.assertTrue(
+            all(
+                not invoice.amount_residual
+                for invoice in order.invoice_ids.filtered(
+                    lambda invoice: invoice.state == "posted"
+                )
+            )
+        )
+        self.assertEqual(order.travel_payment_state, "paid")
+        self.assertEqual(self.departure.reserved_seats, 2)
+
+    def test_posted_unpaid_final_invoice_remains_dp(self):
+        order = self._confirmed_booking(
+            "PAY-FINAL-UNPAID", participant_count=2
+        )
+        downpayment_invoice = self._create_downpayment_invoice(order)
+        self._post_and_pay(downpayment_invoice)
+        final_invoice = self._create_final_invoice(order)
+
+        final_invoice.with_user(self.finance).action_post()
+        order.invalidate_recordset(
+            ["invoice_ids", "invoice_status", "travel_payment_state"]
+        )
+
+        self.assertGreater(final_invoice.amount_residual, 0)
         self.assertEqual(order.travel_payment_state, "dp")
 
     def test_ordinary_sales_order_has_no_travel_payment_state(self):
